99610b816575083718c4013a6b03fc72f2f2c73e
Nate Rodrigues <naterodrigues@Nates-MacBook-Pro.local>
2022-01-27 20:45:24 +1100

    Adding Github API and creating Avatar Cards on results page.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
