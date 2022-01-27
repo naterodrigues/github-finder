5d33ccc9cfee7234b224cc6656334dff04a10bb7
Nate Rodrigues <naterodrigues@Nates-MacBook-Pro.local>
2022-01-27 18:29:20 +1100

    Adding Home, About and Not Found Pages as well as Navbar and Footer layout

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -16,10 +16,10 @@ function Navbar({ title }) {
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/home" className="btn btn-ghost btn-sm rounded-btn">
               Home
             </Link>
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
               About
             </Link>
           </div>
