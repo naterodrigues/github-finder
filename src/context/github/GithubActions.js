89c706f924c98cec00866d346f0dff3d78981cbb
Nate Rodrigues <naterodrigues@Nates-MacBook-Pro.local>
2022-01-29 09:45:41 +1100

    Cleaning up profile images

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -16,6 +16,7 @@ export const searchUsers = async (text) => {
   const response = await github.get(`/search/users?${params}`);
   return response.data.items;
 };
+
 // Get user and repos
 export const getUserAndRepos = async (login) => {
   const [user, repos] = await Promise.all([
